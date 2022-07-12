1a68f324d2c59a90850df15a765c5ed6686b191b
SANYASINGH02 <98136537+SANYASINGH02@users.noreply.github.com>
2022-07-12 06:02:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let secBox = document.getElementById("sec-box");
 
 //Format: Date(year, month, day, hour, minute)
 //Year is counter from 0 to 11
-let endDate = new Date(2022, 6, 11, 23, 45,50);
+let endDate = new Date(2022, 6, 12, 06, 10,00);
 //Output value in milliseconds
 let endTime = endDate.getTime();
 
